6468c7344d8f4ba6018cfdc2a4b6e5e98196bb11
Dani <danimrjm96@gmail.com>
2023-12-13 18:54:10 +0100

    cv

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { CalendarComponent } from './calendar/calendar.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent,
+    CalendarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [
+    // no need to place any providers due to the `providedIn` flag...
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
