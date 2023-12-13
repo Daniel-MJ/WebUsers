1f8e18040ae7ee0fd3f8d5e428169e3fb46d56d6
Dani <danimrjm96@gmail.com>
2023-12-13 19:08:20 +0100

    cv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
-
+const routes: Routes = [
+  { path: 'serach', component: SearchComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
