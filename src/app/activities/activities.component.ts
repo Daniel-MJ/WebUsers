b6746fc7b63f77073a62fdf18ac39c1fa1d468ee
Dani <danimrjm96@gmail.com>
2023-12-14 21:00:58 +0100

    cv

--- src/app/activities/activities.component.ts
+++ src/app/activities/activities.component.ts
@@ -1,12 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { Insputssearch } from '../insputssearch'; 
 
 @Component({
   selector: 'app-activities',
-  standalone: true,
-  imports: [],
   templateUrl: './activities.component.html',
   styleUrl: './activities.component.css'
 })
