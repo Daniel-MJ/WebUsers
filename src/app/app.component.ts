9a6301870aba983e4be603dbd72b8a11ff18e0e4
Dani <danimrjm96@gmail.com>
2024-01-19 18:02:11 +0100

    cv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ETS. INGENIERÍAS INFORMÁTICA Y DE TELECOMUNICACIÓN';
+  title = 'Actividades ETSIIT';
 }
